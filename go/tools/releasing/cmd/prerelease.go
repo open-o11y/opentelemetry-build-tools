76bb06ef885916a478c4ef0df5570204b5cbbddc
Eddy Lin <elindy26@gmail.com>
2021-07-01 14:26:29 -0400

    Replaced strings.Replace with QuoteMeta

--- go/tools/releasing/cmd/prerelease.go
+++ go/tools/releasing/cmd/prerelease.go
@@ -304,6 +304,6 @@ func (p prerelease) updateAllGoModFiles() error {
 
 func filePathToRegex(fpath string) string {
 	replacedSlashes := strings.Replace(fpath, string(filepath.Separator), `\/`, -1)
-	replacedPeriods := strings.Replace(replacedSlashes, ".", `\.`, -1)
-	return replacedPeriods
+	pathRegex := regexp.QuoteMeta(replacedSlashes)
+	return pathRegex
 }
